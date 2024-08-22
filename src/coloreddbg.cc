6c1b5d42d90cf4baca3a2505fb05acac9e96ec26
Jie Yang <yangjie8918@gmail.com>
2024-08-21 17:41:11 -0700

    adjust space

--- src/coloreddbg.cc
+++ src/coloreddbg.cc
@@ -55,22 +55,24 @@ build_main ( BuildOpts& opt )
 {
 	spdlog::logger* console = opt.console.get();
 	std::ifstream infile(opt.inlist);
-  uint64_t num_samples{0};
-  if (infile.is_open()) {
-    std::string line;
-    while (std::getline(infile, line)) { ++num_samples; }
-    infile.clear();
-    infile.seekg(0, std::ios::beg);
-    console->info("Will build mantis index over {} input experiments.", num_samples);
-  } else {
-    console->error("Input file {} does not exist or could not be opened.", opt.inlist);
-    std::exit(1);
-  }
+  	uint64_t num_samples{0};
+  	if (infile.is_open()) {
+    	std::string line;
+    	while (std::getline(infile, line)) {
+			++num_samples;
+		}
+    	infile.clear();
+    	infile.seekg(0, std::ios::beg);
+    	console->info("Will build mantis index over {} input experiments.", num_samples);
+  	} else {
+    	console->error("Input file {} does not exist or could not be opened.", opt.inlist);
+    	std::exit(1);
+  	}
 
-  /** try and create the output directory
-   *  and write a file to it.  Complain to the user
-   *  and exit if we cannot.
-   **/
+  	/** try and create the output directory
+   	*  and write a file to it.  Complain to the user
+   	*  and exit if we cannot.
+   	**/
 	std::string prefix(opt.out);
 	if (prefix.back() != '/') {
 		prefix += '/';
@@ -90,24 +92,24 @@ build_main ( BuildOpts& opt )
   {
     std::ofstream jfile(prefix + "/" + mantis::meta_file_name);
     if (jfile.is_open()) {
-      minfo = opt.to_json();
-      minfo["start_time"] = mantis::get_current_time_as_string();
-      minfo["mantis_version"] = mantis::version;
-      minfo["index_version"] = mantis::index_version;
-      jfile << minfo.dump(4);
+      	minfo = opt.to_json();
+      	minfo["start_time"] = mantis::get_current_time_as_string();
+      	minfo["mantis_version"] = mantis::version;
+      	minfo["index_version"] = mantis::index_version;
+      	jfile << minfo.dump(4);
     } else {
-      console->error("Could not write to output directory {}", prefix);
-      exit(1);
+      	console->error("Could not write to output directory {}", prefix);
+      	exit(1);
     }
     jfile.close();
   }
 
 	std::vector<SampleObject<CQF<KeyObject>*>> inobjects;
-  std::vector<CQF<KeyObject>> cqfs;
+  	std::vector<CQF<KeyObject>> cqfs;
 
 	// reserve QF structs for input CQFs
-  inobjects.reserve(num_samples);
-  cqfs.reserve(num_samples);
+  	inobjects.reserve(num_samples);
+  	cqfs.reserve(num_samples);
 
 	// mmap all the input cqfs
 	std::string squeakr_file;
@@ -130,8 +132,9 @@ build_main ( BuildOpts& opt )
 			console->error("Can't read Squeakr file. It was written on a different endian machine.");
 			exit(1);
 		}
-		if (cqfs.size() == 0)
+		if (cqfs.size() == 0) {
 			kmer_size = config.kmer_size;
+		}
 		else {
 			if (kmer_size != config.kmer_size) {
 				console->error("Squeakr file {} has a different k-mer size. Expected: {} Available: {}",
@@ -143,30 +146,30 @@ build_main ( BuildOpts& opt )
 			console->warn("Squeakr file {} is not filtered.", squeakr_file);
 		}
 
-    cqfs.emplace_back(squeakr_file, CQF_MMAP);
+    	cqfs.emplace_back(squeakr_file, CQF_MMAP);
 		//std::string sample_id = first_part(first_part(last_part(squeakr_file, '/'),
 																									//'.'), '_');
 		std::string sample_id = squeakr_file;
 		console->info("Reading CQF {} Seed {}",nqf, cqfs[nqf].seed());
 		console->info("Sample id {}", sample_id);
 		cqfs.back().dump_metadata();
-    inobjects.emplace_back(&cqfs[nqf], sample_id, nqf);
+    	inobjects.emplace_back(&cqfs[nqf], sample_id, nqf);
 		if (!cqfs.front().check_similarity(&cqfs.back())) {
 			console->error("Passed Squeakr files are not similar.", squeakr_file);
 			exit(1);
 		}
-    nqf++;
+    	nqf++;
 	}
 
 	ColoredDbg<SampleObject<CQF<KeyObject>*>, KeyObject> cdbg(opt.qbits,
-																														inobjects[0].obj->keybits(),
-																														cqfs[0].hash_mode(),
-																														inobjects[0].obj->seed(),
-																														prefix, nqf, MANTIS_DBG_ON_DISK);
+															inobjects[0].obj->keybits(),
+															cqfs[0].hash_mode(),
+															inobjects[0].obj->seed(),
+															prefix, nqf, MANTIS_DBG_ON_DISK);
 	cdbg.set_console(console);
 	if (opt.flush_eqclass_dist) {
 		cdbg.set_flush_eqclass_dist();
-  }
+  	}
 
 	cdbg.build_sampleid_map(inobjects.data());
 
